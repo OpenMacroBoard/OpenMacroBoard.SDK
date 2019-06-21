f8367fcfe73c7288b5ab20635912fd0935433e20
Christian Wischenbart <c.wischenbart@bitoxide.com>
2019-06-21 07:26:47 +0200

    Add test for ConnectionEventArgs

---
+++ src/OpenMacroBoard.Tests/ConnectionEventArgsTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using OpenMacroBoard.SDK;
+using Xunit;
+
+namespace OpenMacroBoard.Tests
+{
+    public class ConnectionEventArgsTest
+    {
+        [Fact]
+        public void NewConnectionPropertyShouldBeSet()
+        {
+            var con = new ConnectionEventArgs(true);
+            con.NewConnectionState.Should().BeTrue();
+
+            con = new ConnectionEventArgs(false);
+            con.NewConnectionState.Should().BeFalse();
+        }
+    }
+}
